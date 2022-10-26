e9dc76164a0b2fa14fe4ef2e4c945c5459148490
Renat Gubaydullin <renatgubaudullin@gmail.com>
2022-10-26 12:51:29 +0300

    v 1.3

--- src/utils/uuid.js
+++
@@ -1,4 +0,0 @@
-import crypto from 'crypto-browserify';
-
-// генерируем какой-то идентификатор для запуска
-export const UUID = crypto.randomUUID();
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,117 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
+/* eslint-disable quotes */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const StatoscopePlugin = require("@statoscope/webpack-plugin").default;
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
 
 const config = {
-    entry: {
-        about: './src/pages/About.js',
-        home: './src/pages/Home.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new StatoscopePlugin({
-            saveStatsTo: 'stats.json',
-            saveOnlyStats: false,
-            open: false,
-        }),
+  entry: {
+    about: "./src/pages/About.js",
+    TodoItem: "./src/components/TodoItem.js",
+    TodoList: { import: "./src/components/TodoList.js", dependOn: ["TodoItem"] },
+    home: { import: "./src/pages/Home.js", dependOn: ["TodoList"] },
+    index: { import: "./src/index.js", dependOn: ["about", "home"] },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // путь к файлу index.html
+    }),
+    new StatoscopePlugin({
+      saveStatsTo: "stats.json",
+      saveOnlyStats: false,
+      open: false,
+    }),
+    new LodashModuleReplacementPlugin({
+      collections: true,
+      paths: true,
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+  mode: "production",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        use: "babel-loader",
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".js"],
+        },
+        sideEffects: false,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
     ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
+  },
+  optimization: {
+    usedExports: true,
+    concatenateModules: true,
+    minimize: true,
+    moduleIds: "deterministic",
+    innerGraph: true,
+    providedExports: true,
+    splitChunks: {
+      minSize: 20000,
+      minRemainingSize: 0,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      minChunks: 1,
+      chunks: "all",
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
-    module: {
-        rules: [
-            // @TODO js rule
-            // @TODO css rule
-        ],
+    runtimeChunk: {
+      name: "runtime",
     },
-    // @TODO optimizations
-    // @TODO lodash treeshaking
-    // @TODO chunk for lodash
-    // @TODO chunk for runtime
-    // @TODO fallback for crypto
+  },
+  target: "web",
+
+  resolve: {
+    alias: {
+      "crypto-browserify": path.resolve(__dirname, "src/crypto-fall.js"),
+    },
+    fallback: {
+      buffer: require.resolve("buffer"),
+      stream: false,
+      crypto: require.resolve("crypto"),
+    },
+    modules: [
+      path.resolve(__dirname, "node_modules"),
+      path.resolve(__dirname, "node_modules/ui/node_modules"),
+    ],
+  },
+  performance: {
+    hints: false,
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000,
+  },
+  stats: {
+    children: true,
+  },
+
 };
 
 module.exports = config;
